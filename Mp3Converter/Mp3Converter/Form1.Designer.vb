19758d3a9493534f288f90f1df4e3993347e44fa
EZIOxtn <156960899+EZIOxtn@users.noreply.github.com>
2024-10-12 14:47:37 +0100

    Add files via upload

---
+++ Mp3Converter/Mp3Converter/Form1.Designer.vb
@@ -0,0 +1,201 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Guna2ShadowPanel1 = New Guna.UI2.WinForms.Guna2ShadowPanel()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.Guna2HtmlLabel3 = New Guna.UI2.WinForms.Guna2HtmlLabel()
+        Me.Guna2HtmlLabel2 = New Guna.UI2.WinForms.Guna2HtmlLabel()
+        Me.Guna2CustomCheckBox1 = New Guna.UI2.WinForms.Guna2CustomCheckBox()
+        Me.Guna2GradientButton1 = New Guna.UI2.WinForms.Guna2GradientButton()
+        Me.Guna2CustomGradientPanel1 = New Guna.UI2.WinForms.Guna2CustomGradientPanel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.mpsel = New System.Windows.Forms.OpenFileDialog()
+        Me.Guna2ShadowPanel1.SuspendLayout()
+        Me.Guna2CustomGradientPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Guna2ShadowPanel1
+        '
+        Me.Guna2ShadowPanel1.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2ShadowPanel1.Controls.Add(Me.LinkLabel1)
+        Me.Guna2ShadowPanel1.Controls.Add(Me.Guna2HtmlLabel3)
+        Me.Guna2ShadowPanel1.Controls.Add(Me.Guna2HtmlLabel2)
+        Me.Guna2ShadowPanel1.Controls.Add(Me.Guna2CustomCheckBox1)
+        Me.Guna2ShadowPanel1.Controls.Add(Me.Guna2GradientButton1)
+        Me.Guna2ShadowPanel1.Controls.Add(Me.Guna2CustomGradientPanel1)
+        Me.Guna2ShadowPanel1.FillColor = System.Drawing.Color.Black
+        Me.Guna2ShadowPanel1.Location = New System.Drawing.Point(3, 1)
+        Me.Guna2ShadowPanel1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Guna2ShadowPanel1.Name = "Guna2ShadowPanel1"
+        Me.Guna2ShadowPanel1.Radius = 10
+        Me.Guna2ShadowPanel1.ShadowColor = System.Drawing.Color.Maroon
+        Me.Guna2ShadowPanel1.ShadowShift = 18
+        Me.Guna2ShadowPanel1.Size = New System.Drawing.Size(803, 405)
+        Me.Guna2ShadowPanel1.TabIndex = 0
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(249, 348)
+        Me.LinkLabel1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(39, 17)
+        Me.LinkLabel1.TabIndex = 6
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Here"
+        '
+        'Guna2HtmlLabel3
+        '
+        Me.Guna2HtmlLabel3.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2HtmlLabel3.Font = New System.Drawing.Font("Lucida Sans Unicode", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Guna2HtmlLabel3.ForeColor = System.Drawing.Color.Maroon
+        Me.Guna2HtmlLabel3.Location = New System.Drawing.Point(41, 346)
+        Me.Guna2HtmlLabel3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Guna2HtmlLabel3.Name = "Guna2HtmlLabel3"
+        Me.Guna2HtmlLabel3.Size = New System.Drawing.Size(201, 19)
+        Me.Guna2HtmlLabel3.TabIndex = 5
+        Me.Guna2HtmlLabel3.Text = "change the output path from"
+        '
+        'Guna2HtmlLabel2
+        '
+        Me.Guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2HtmlLabel2.Font = New System.Drawing.Font("Lucida Sans Unicode", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Guna2HtmlLabel2.ForeColor = System.Drawing.Color.Maroon
+        Me.Guna2HtmlLabel2.Location = New System.Drawing.Point(68, 319)
+        Me.Guna2HtmlLabel2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Guna2HtmlLabel2.Name = "Guna2HtmlLabel2"
+        Me.Guna2HtmlLabel2.Size = New System.Drawing.Size(188, 19)
+        Me.Guna2HtmlLabel2.TabIndex = 4
+        Me.Guna2HtmlLabel2.Text = "Open path after converting"
+        '
+        'Guna2CustomCheckBox1
+        '
+        Me.Guna2CustomCheckBox1.Animated = True
+        Me.Guna2CustomCheckBox1.CheckedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Guna2CustomCheckBox1.CheckedState.BorderRadius = 2
+        Me.Guna2CustomCheckBox1.CheckedState.BorderThickness = 0
+        Me.Guna2CustomCheckBox1.CheckedState.FillColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Guna2CustomCheckBox1.CheckedState.Parent = Me.Guna2CustomCheckBox1
+        Me.Guna2CustomCheckBox1.Location = New System.Drawing.Point(41, 319)
+        Me.Guna2CustomCheckBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Guna2CustomCheckBox1.Name = "Guna2CustomCheckBox1"
+        Me.Guna2CustomCheckBox1.ShadowDecoration.Parent = Me.Guna2CustomCheckBox1
+        Me.Guna2CustomCheckBox1.Size = New System.Drawing.Size(19, 20)
+        Me.Guna2CustomCheckBox1.TabIndex = 3
+        Me.Guna2CustomCheckBox1.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(125, Byte), Integer), CType(CType(137, Byte), Integer), CType(CType(149, Byte), Integer))
+        Me.Guna2CustomCheckBox1.UncheckedState.BorderRadius = 2
+        Me.Guna2CustomCheckBox1.UncheckedState.BorderThickness = 0
+        Me.Guna2CustomCheckBox1.UncheckedState.FillColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Guna2CustomCheckBox1.UncheckedState.Parent = Me.Guna2CustomCheckBox1
+        '
+        'Guna2GradientButton1
+        '
+        Me.Guna2GradientButton1.Animated = True
+        Me.Guna2GradientButton1.BorderColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Guna2GradientButton1.BorderThickness = 1
+        Me.Guna2GradientButton1.CheckedState.Parent = Me.Guna2GradientButton1
+        Me.Guna2GradientButton1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Guna2GradientButton1.CustomImages.Parent = Me.Guna2GradientButton1
+        Me.Guna2GradientButton1.FillColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Guna2GradientButton1.FillColor2 = System.Drawing.Color.Black
+        Me.Guna2GradientButton1.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Guna2GradientButton1.ForeColor = System.Drawing.Color.White
+        Me.Guna2GradientButton1.HoverState.BorderColor = System.Drawing.Color.Maroon
+        Me.Guna2GradientButton1.HoverState.FillColor = System.Drawing.Color.Black
+        Me.Guna2GradientButton1.HoverState.FillColor2 = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Guna2GradientButton1.HoverState.Parent = Me.Guna2GradientButton1
+        Me.Guna2GradientButton1.Location = New System.Drawing.Point(435, 319)
+        Me.Guna2GradientButton1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Guna2GradientButton1.Name = "Guna2GradientButton1"
+        Me.Guna2GradientButton1.ShadowDecoration.Parent = Me.Guna2GradientButton1
+        Me.Guna2GradientButton1.Size = New System.Drawing.Size(240, 55)
+        Me.Guna2GradientButton1.TabIndex = 2
+        Me.Guna2GradientButton1.Text = "Convert to OGG"
+        '
+        'Guna2CustomGradientPanel1
+        '
+        Me.Guna2CustomGradientPanel1.AllowDrop = True
+        Me.Guna2CustomGradientPanel1.BorderColor = System.Drawing.Color.White
+        Me.Guna2CustomGradientPanel1.BorderStyle = System.Drawing.Drawing2D.DashStyle.Dash
+        Me.Guna2CustomGradientPanel1.Controls.Add(Me.Label1)
+        Me.Guna2CustomGradientPanel1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Guna2CustomGradientPanel1.FillColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Guna2CustomGradientPanel1.FillColor2 = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Guna2CustomGradientPanel1.FillColor3 = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Guna2CustomGradientPanel1.FillColor4 = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Guna2CustomGradientPanel1.Location = New System.Drawing.Point(65, 44)
+        Me.Guna2CustomGradientPanel1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Guna2CustomGradientPanel1.Name = "Guna2CustomGradientPanel1"
+        Me.Guna2CustomGradientPanel1.ShadowDecoration.Parent = Me.Guna2CustomGradientPanel1
+        Me.Guna2CustomGradientPanel1.Size = New System.Drawing.Size(675, 249)
+        Me.Guna2CustomGradientPanel1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AllowDrop = True
+        Me.Label1.Font = New System.Drawing.Font("Impact", 43.0!)
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(7, 64)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(663, 89)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Drop your mp3 here"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'mpsel
+        '
+        Me.mpsel.FileName = "Mp3"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(803, 407)
+        Me.Controls.Add(Me.Guna2ShadowPanel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "conv"
+        Me.Guna2ShadowPanel1.ResumeLayout(False)
+        Me.Guna2ShadowPanel1.PerformLayout()
+        Me.Guna2CustomGradientPanel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Guna2ShadowPanel1 As Guna.UI2.WinForms.Guna2ShadowPanel
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents Guna2HtmlLabel3 As Guna.UI2.WinForms.Guna2HtmlLabel
+    Friend WithEvents Guna2HtmlLabel2 As Guna.UI2.WinForms.Guna2HtmlLabel
+    Friend WithEvents Guna2CustomCheckBox1 As Guna.UI2.WinForms.Guna2CustomCheckBox
+    Friend WithEvents Guna2GradientButton1 As Guna.UI2.WinForms.Guna2GradientButton
+    Friend WithEvents Guna2CustomGradientPanel1 As Guna.UI2.WinForms.Guna2CustomGradientPanel
+    Friend WithEvents mpsel As OpenFileDialog
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Mp3Converter/Mp3Converter/Form1.vb
@@ -0,0 +1,130 @@
+﻿Imports System.IO
+Imports NAudio.Wave
+
+Public Class Form1
+    Public gex As String
+    Public bol As Boolean = False
+    Sub Main()
+        InitializeComponent()
+    End Sub
+    Private Sub Guna2CustomGradientPanel1_Paint(sender As Object, e As DragEventArgs) Handles Guna2CustomGradientPanel1.DragDrop
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            Dim files As String() = CType(e.Data.GetData(DataFormats.FileDrop), String())
+
+            If files.Length > 0 Then
+                Dim filePath As String = files(0)
+
+                Label1.Text = "Ready"
+                gex = filePath
+                bol = True
+            End If
+        End If
+
+    End Sub
+    Private Sub Guna2CstomGradientPanel1_Paint(sender As Object, e As DragEventArgs) Handles Guna2CustomGradientPanel1.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        Else
+            e.Effect = DragDropEffects.None
+        End If
+        Guna2CustomGradientPanel1.FillColor = Color.Black
+        Guna2CustomGradientPanel1.FillColor4 = Color.Black
+    End Sub
+    Private Sub ld(sender As Object, e As EventArgs) Handles MyBase.Load
+        My.Settings.pth = Application.StartupPath
+    End Sub
+    Private Sub Guna2CustomGradientPaint(sender As Object, e As DragEventArgs) Handles Label1.DragDrop
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            Dim files As String() = CType(e.Data.GetData(DataFormats.FileDrop), String())
+
+            If files.Length > 0 Then
+                Dim filePath As String = files(0)
+
+
+                Label1.Text = "Ready"
+                gex = filePath
+                bol = True
+            End If
+        End If
+
+    End Sub
+    Private Sub Guna2CstomGradienl1_Paint(sender As Object, e As DragEventArgs) Handles Label1.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        Else
+            e.Effect = DragDropEffects.None
+        End If
+        Guna2CustomGradientPanel1.FillColor = Color.Black
+        Guna2CustomGradientPanel1.FillColor4 = Color.Black
+    End Sub
+    Private Sub Guna2CstomGradientPanel1_Pat(sender As Object, e As DragEventArgs) Handles Guna2CustomGradientPanel1.DragLeave
+        Guna2CustomGradientPanel1.FillColor = Color.FromArgb(64, 0, 0)
+        Guna2CustomGradientPanel1.FillColor4 = Color.FromArgb(0, 0, 64)
+    End Sub
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles Guna2GradientButton1.Click
+        If Not bol Then
+            MsgBox("select a file first")
+            Exit Sub
+        End If
+        If Not String.IsNullOrEmpty(gex) Then
+            Dim inputFile As String = gex
+            Dim outputFileName As String = Path.GetFileNameWithoutExtension(inputFile) & ".ogg"
+            Dim outputPath As String = Path.Combine(My.Settings.pth, outputFileName)
+            If File.Exists(inputFile) Then
+                If Path.GetExtension(inputFile).Equals(".mp3", StringComparison.OrdinalIgnoreCase) Then
+                    Try
+                        Using reader As New Mp3FileReader(inputFile)
+                            Using writer As New WaveFileWriter(outputPath, reader.WaveFormat)
+                                Dim buffer(1024) As Byte
+                                Dim bytesRead As Integer
+                                Dim totalBytesRead As Long = 0
+                                Dim totalBytes As Long = reader.Length
+
+                                Do
+                                    bytesRead = reader.Read(buffer, 0, buffer.Length)
+                                    writer.Write(buffer, 0, bytesRead)
+                                    totalBytesRead += bytesRead
+                                    Dim progressPercentage As Integer = CInt((totalBytesRead / totalBytes) * 100)
+                                    Label1.Text = $"Progress: {progressPercentage}%"
+                                    Application.DoEvents()
+                                Loop While bytesRead > 0
+                            End Using
+                        End Using
+
+                        Label1.Text = "finish"
+                        Guna2CustomGradientPanel1.FillColor = Color.FromArgb(64, 0, 0)
+                        Guna2CustomGradientPanel1.FillColor4 = Color.FromArgb(0, 0, 64)
+                        Threading.Thread.Sleep(1000)
+                        gex = ""
+                        bol = False
+                        Guna2CustomGradientPanel1.FillColor = Color.FromArgb(64, 0, 0)
+                        Guna2CustomGradientPanel1.FillColor4 = Color.FromArgb(0, 0, 64)
+
+                        Label1.Text = "Drop your mp3 here"
+                    Catch ex As Exception
+                        MessageBox.Show($"Error during conversion: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    End Try
+
+                Else
+                    MessageBox.Show("Only MP3 files can be converted to OGG.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                End If
+            Else
+                MessageBox.Show("File not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        Else
+            Label1.Text = "Select a file first"
+        End If
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Dim op As New FolderBrowserDialog()
+        If op.ShowDialog() = DialogResult.OK Then
+            If op IsNot Nothing Then
+                My.Settings.pth = op.SelectedPath
+            Else
+                MsgBox("error", "no path selected")
+            End If
+        End If
+
+    End Sub
+End Class
